a3df145f6cdfb22af8a6e523dd3afdd56d10da2a
Ajeet Kumar <akumar010.btech2022@ece.nitrr.ac.in>
2025-07-11 00:25:27 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -56,7 +56,7 @@ app.get('/', (req, res) => {
 });
 
 // Connect to MongoDB and start server
-const PORT =  5000;
+const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
 mongoose.connect(MONGO_URI, {
